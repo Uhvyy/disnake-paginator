347e33d745fd04ed51715dcae685454846f19b23
ErrorNoInternet <error.nointernet@gmail.com>
2021-10-31 17:26:24 +0800

    Initial commit

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     long_description_content_type='text/markdown',
     author = 'Ryan Huang',
     url = 'https://github.com/ErrorNoInternet/disnake-paginator',
-    download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+    download_url = 'https://github.com/ErrorNoInternet/disnake-paginator/archive/refs/tags/0.1.tar.gz',
     keywords = ["discord", "disnake", "paginator"],
     install_requires = [
         "disnake",
